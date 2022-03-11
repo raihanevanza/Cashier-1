0f36580a98745d6eb3f2606e5eb8cb2c8958290e
AnugrahPerdana <anugrahperdana06@gmail.com>
2022-03-11 17:55:37 +0700

    test commit

--- application/views/Kasir/order.php
+++ application/views/Kasir/order.php
@@ -6,134 +6,87 @@
 					<div class="content-main-parent">
 						<div class="row justify-content-left">
 							<div class="col-2 image-chevron">
-						      	<a href="">
-									<img src="<?php echo base_url('asset/image/content/chevronBack.png') ?>">	
+								<a href="">
+									<img src="<?php echo base_url('asset/image/content/chevronBack.png') ?>">
 								</a>
-						    </div>
-						    <div class="col-md-offset-4 col-md-10 search-parent">
-						    	<form>
-								  <div class="form-group has-search search-group">
-								    <span class="fa fa-search form-control-feedback search-span "></span>
-								    <input type="text" class="form-control search-input" placeholder="Search">
-								  </div>
+							</div>
+							<div class="col-md-offset-4 col-md-9 search-parent">
+								<form>
+									<div class="form-group has-search search-group">
+										<span class="fa fa-search form-control-feedback search-span "></span>
+										<input type="text" class="form-control search-input" placeholder="| Search">
+									</div>
 								</form>
-						    </div>
-						    <div class="col-md-offset-8 col-md-12">
-						    	<div class="col-4 image-category">
-							      	<a href="">
+							</div>
+							<div class="col-md-offset-4 col-md-12">
+								<div class="col-5 image-category">
+									<a href="">
 										<img src="<?php echo base_url('asset/image/menu/coffee.png') ?>">
 									</a>
-							    </div>
-							    <div class="col-4 image-category">
-							      	<a href="">
-										<img src="<?php echo base_url('asset/image/menu/drink.png') ?>">	
+								</div>
+								<div class="col-5 image-category">
+									<a href="">
+										<img src="<?php echo base_url('asset/image/menu/drink.png') ?>">
 									</a>
-							    </div>
-							    <div class="col-4 image-category">
-							      	<a href="">
+								</div>
+								<div class="col-5 image-category">
+									<a href="">
 										<img src="<?php echo base_url('asset/image/menu/milky.png') ?>">
 									</a>
-							    </div>
-							    <div class="col-4 image-category">
-							      	<a href="">
+								</div>
+								<div class="col-3 image-category">
+									<a href="">
 										<img src="<?php echo base_url('asset/image/menu/food.png') ?>">
 									</a>
-							    </div>
-						    </div>
-						    <div class="col-12 col-md-offset-2 main-menu-content">
-								<div class="content-main-parent">
-									<div class="row justify-content-left">
-										<div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/coffee-menu.png') ?>">	
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/coffee-menu.png') ?>">	
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
-								    <div class="col col-md-3 image-menu">
-								      	<a href="">
-											<img src="<?php echo base_url('asset/image/menu/kopsus-image.png') ?>">
-										</a>
-										<h1 class="title-order-menu">Classic Coffee</h1>
-										<p class="deskipsi">Americano with ice and dolce souce.</p>
-										<span class="harga-menu">
-											Rp 27.000
-											<img src="<?php echo base_url('asset/image/menu/chevron-menu.png') ?>">
-										</span>
-								    </div>
+								</div>
+							</div>
+							<div class="col col-sm-11 mb-4 image-menu">
+								<div class="card-body bg-light">
+									<div class="card-text mb-1">Coffee
+									</div>
+									<div class="row">
+										<div class="col-sm-10">
+											<h5 class="card-title">Kopi Susu Guljaw</h5>
+										</div>
+										<div class="card-title col-sm-2">14.000</div>
 									</div>
 								</div>
 							</div>
+							<div class="col col-sm-11 mb-4 image-menu">
+								<div class="card-body bg-light">
+									<div class="card-text mb-1">Coffee</div>
+									<div class="row">
+										<div class="col-sm-10">
+											<h5 class="card-title">Kopi Susu Melontea</h5>
+										</div>
+										<div class="card-title col-sm-2">14.000</div>
+									</div>
+								</div>
+							</div>
+							<div class="col col-sm-11 mb-4 image-menu">
+								<div class="card-body bg-light">
+									<div class="card-text mb-1">Coffee</div>
+									<div class="row">
+										<div class="col-sm-10">
+											<h5 class="card-title">Kopi Susu Vanilla</h5>
+										</div>
+										<div class="card-title col-sm-2">14.000</div>
+									</div>
+								</div>
+							</div>
+							<div class="col col-sm-11 mb-4 image-menu">
+								<div class="card-body bg-light">
+									<div class="card-text mb-1">Coffee</div>
+									<div class="row">
+										<div class="col-sm-10">
+											<h5 class="card-title">Kopi Susu Original</h5>
+										</div>
+										<div class="card-title col-sm-2">14.000</div>
+									</div>
+								</div>
+							</div>
+
+
 						</div>
 					</div>
 				</div>
@@ -141,97 +94,99 @@
 					<div class="">
 						<div class="row justify-content-center">
 							<div class="card shadow mb-4 card-dash card-detail-order">
-					            <!-- Card Header - Accordion -->
-					            <a href="#collapseCardStock" class="d-block card-header py-3 card-header-menu" data-toggle="collapse"
-					                role="button" aria-expanded="true" aria-controls="collapseCardStock">
-					                <div class="title-cards">
-					                	Detail Pesanan
-					                </div>
-					                <div class="title-cards-sub">
-					                	Pesanan No. 31
-					                </div>
-					            </a>
-					            <!-- Card Content - Collapse -->
-					            <div class="collapse show collapse-parent-detail" id="collapseCardStock">
-					                <table class="table table-striped">
-									  <thead>
-									    <tr>
-									      <th scope="col">Menu</th>
-									      <th scope="col">Jumlah</th>
-									      <th scope="col">Jumlah</th>
-									    </tr>
-									  </thead>
-									  <tbody>
-									    <tr>
-									      <th scope="row">1</th>
-									      <td>Mark</td>
-									      <td>Otto</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">2</th>
-									      <td>Jacob</td>
-									      <td>Thornton</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">3</th>
-									      <td>Larry</td>
-									      <td>the Bird</td>
-									    </tr>
-									  </tbody>
+								<!-- Card Header - Accordion -->
+								<a href="#collapseCardStock" class="d-block card-header py-3 card-header-menu" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardStock">
+									<div class="title-cards">
+										Detail Pesanan
+									</div>
+									<div class="d-flex justify-content-between mt-2">
+										<div class="title-cards-sub">
+											Pesanan No. 31
+										</div>
+										<div>
+											<i class="fa-solid fa-plus"></i>
+										</div>
+									</div>
+
+								</a>
+								<!-- Card Content - Collapse -->
+								<div class="collapse show collapse-parent-detail" id="collapseCardStock">
+									<table class="table no-border" id="table" data-toggle="table">
+										<thead>
+											<tr>
+												<th data-order='desc' data-field="id" data-sortable="true">Menu</th>
+												<th data-order='desc' data-field="name" data-sortable="true">Harga</th>
+												<th data-order='desc' data-field="price" data-sortable="true">Jumlah</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>Otak-otak</td>
+												<td>13.000</td>
+												<td><i class='fas fa-circle-minus'></i> 1 <i class="fa-solid fa-circle-plus"></i></td>
+											</tr>
+											<tr>
+												<td>Mineral Water</td>
+												<td>5000</td>
+												<td><i class='fas fa-circle-minus'></i> 2 <i class="fa-solid fa-circle-plus"></i></td>
+											</tr>
+											<tr>
+												<td>Americano</td>
+												<td>12.000</td>
+												<td><i class='fas fa-circle-minus'></i> 1 <i class="fa-solid fa-circle-plus"></i></td>
+											</tr>
+										</tbody>
 									</table>
-					            </div>
-					        </div>
-					        <div class="card-dash">
-								<div class="form-group">	
-							    	<span class="h6 small bg-white text-muted pt-1 pl-2 pr-2 label-order">
-							    		Name<span class="require-notice">*</span>
-							    	</span>
+								</div>
+							</div>
+							<div class="card-dash">
+								<div class="form-group">
+									<span class="h6 small bg-white text-muted pt-1 pl-2 pr-2 label-order">
+										Name<span class="require-notice">*</span>
+									</span>
 									<input type="text" class="form-control mt-n3 order-input" id="input1" placeholder="Nama Pelanggan">
 								</div>
-								<div class="form-group">	
-							    	<span class="h6 small bg-white text-muted pt-1 pl-2 pr-2 label-order">
-							    		Email atau Nomor Handphone<span class="require-notice">*</span>
-							    	</span>
+								<div class="form-group">
+									<span class="h6 small bg-white text-muted pt-1 pl-2 pr-2 label-order">
+										Email atau Nomor Handphone<span class="require-notice">*</span>
+									</span>
 									<input type="text" class="form-control mt-n3 order-input" id="input1" placeholder="Nama Pelanggan">
 								</div>
 								<div>
-									<button type="button" class="btn btn-primary btn-order-payment">Metode Pembayaran</button>
+									<div class="d-flex justify-content-between mt-4">
+										<div>
+											Total
+										</div>
+										<h6>
+											Rp.100.000
+										</h6>
+									</div>
+									<div class="d-flex justify-content-between mt-2">
+										<div>
+											Service Charge
+										</div>
+										<h6>
+											1000
+										</h6>
+									</div>
+									<div class="d-flex justify-content-between mt-2">
+										<div>
+											Total Payment
+										</div>
+										<h6>
+											Rp.101.000
+										</h6>
+									</div>
+
 								</div>
-					        </div>
+								<div>
+									<button type="button" class="btn btn-primary btn-order-payment mt-4">Metode Pembayaran</button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>	
+</div>
--- application/views/index.php
+++ application/views/index.php
@@ -1,28 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Cashier</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/bootstrap-4.3.1.css') ?>">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/custome-style.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/table.css') ?>">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css" rel="stylesheet" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
 </head>
-<body class="hero-anime">	
+
+<body class="hero-anime">
 
 	<div class="navigation-wrap bg-light start-header start-style">
 		<div class="container">
 			<div class="row">
 				<div class="col-12">
 					<nav class="navbar navbar-expand-md navbar-light">
-					
+
 						<a class="navbar-brand" href="https://front.codes/" target="_blank">
 							<img src="<?php echo base_url('asset/image/Hore-logo.png') ?>" alt="">
-						</a>	
-						
+						</a>
+
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						</button>
-						
+
 						<div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav ml-auto py-4 py-md-0">
 								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
@@ -30,12 +36,17 @@
 								</li>
 								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
 
-									<a class="nav-link" href="#">
-										<i class="name-profile">Chaidar Abimanyu</i>
-										<img src="<?php echo base_url('asset/image/profile/profil_account.png') ?>" class="img-profile"></i></a>
+									<a class="nav-link " href="#">
+
+										<h6>Chaidar Abimanyu</h6>
+
+									</a>
+									<p class="mr-5">Kopi kenangan Citayam</p>
 								</li>
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 line-manual">
-									<a class="nav-link" href="#"></a>
+								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+									<a class="nav-link mt-2 ml-2" href="#">
+										<img src="<?php echo base_url('asset/image/profile/profil_account.png') ?>" class="img-profile">
+									</a>
 								</li>
 								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
 									<a class="nav-link" href="#"><i class="fa-solid fa-bell nav-icon-custome"></i></a>
@@ -45,16 +56,17 @@
 								</li>
 							</ul>
 						</div>
-						
-					</nav>		
+
+					</nav>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="container-content">
-		<?php $this->load->view($page)?>
+		<?php $this->load->view($page) ?>
 	</div>
 </body>
+
 </html>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
